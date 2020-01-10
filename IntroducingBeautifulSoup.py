77aba2b480d6921754db49f15e4da0bb9125799f
iarafat <miyasinarafat@gmail.com>
2020-01-10 15:41:30 +0600

    worked more with beautiful soup

--- IntroducingBeautifulSoup.py
+++ IntroducingBeautifulSoup.py
@@ -1,5 +1,24 @@
 import requests
 import webbrowser
+import lxml
 
 from bs4 import BeautifulSoup
 
+# response = requests.get('https://google.com')
+# soup = BeautifulSoup(response.text, 'lxml')
+# print(soup)
+
+with open('files/GiantPanda.html') as html_code:
+    soup = BeautifulSoup(html_code, 'lxml')
+
+# print(soup.prettify())
+# print(soup.title)
+
+# print(soup.a)
+# print(soup.a.name)
+# print(soup.a['href'])
+# print(soup.a.attrs)
+
+print(soup.img)
+print(soup.img['src'])
+print(soup.img.attrs)
